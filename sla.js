a92937ba58fa8cad3b1e11ba22cc79fca07fd486
Akyles-Macario <akyles.silva@escola.pr.gov.br>
2025-10-23 10:23:11 -0300

    a

--- sla.js
+++ sla.js
@@ -1,5 +1,16 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('btn-proximo')
+
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
+
  avanca.forEach (button => { 
      button.addEventListener('click', function (){
         
